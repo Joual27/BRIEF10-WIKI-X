2adb366d5fba889c35f3a097d547a055413f6ddb
Joual27 <endy.panda@outlook.com>
2024-01-08 16:38:20 +0100

    MVC structure

---
+++ app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+define('DB_HOST' , 'localhost');
+define('DB_USER' , '_YOUR_USERNAME_');
+define('DB_PASS' , '_YOUR_PASSWORD_');
+define('DB_NAME' , '_YOUR_DATABASE_NAME_');
+define('APPROOT' , dirname(dirname(__FILE__)));
+
+define('URLROOT' , '_YOUR_URL_'); 
+define('SITENAME' , '_YOUR_SITENAME_');
+?>
---
+++ app/views/pages/index.php
@@ -0,0 +1,3 @@
+<?php require APPROOT . '/views/incFile/header.php'; ?>
+<p>Ohayo!</p>
+<?php require APPROOT . '/views/incFile/footer.php'; ?>
